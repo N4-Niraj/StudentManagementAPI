60ff55dde0fce7e3b0577caf1250af34cb801883
N4-Niraj <bhusalniraj03@gmail.com>
2026-08-03 16:13:00 +0545

    Implement student creation and listing endpoints

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from .models import Student
 
 app = FastAPI()
 
@@ -6,3 +7,61 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Welcome to Student Management API"}
+
+@app.get("/students")
+def get_students():
+    return students_db
+
+@app.post("/students")
+def create_student(student: Student):
+    students_db.append(student)
+    return {
+            "message": "student created successfully",
+            "student": student}
+
+
+
+students_db = [
+    Student(
+            id=1,
+            name="Niraj",
+            faculty="Engineering",
+            semester=3,
+            email="niraj@example.com"
+            )
+    ,Student(
+        id=2,
+        name="Ram",
+        faculty="BCA",
+        semester=2,
+        email="alice@example.com"
+        )
+    ,Student(
+        id=3, 
+        name="worm",
+        faculty="Engineering",
+        semester=3,
+        email="worm@example.com"
+        )
+    ,Student(
+        id=4, 
+        name="luke",
+        faculty="BSC-Csit",
+        semester=5,
+        email="luke@example.com"
+        )
+    ,Student(
+        id=5, 
+        name="jay",
+        faculty="BBS",
+        semester=6,
+        email="jay@example.com"
+        )
+    ,Student(
+        id=6, 
+        name="Hari",
+        faculty="BCA",
+        semester=3,
+        email="hari@example.com"
+        )
+    ]
---
+++ app/models.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel 
+
+
+class Student(BaseModel):
+    id: int
+    name: str
+    faculty: str
+    semester: int
+    email: str
