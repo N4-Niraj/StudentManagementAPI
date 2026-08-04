3a7e0090b85baee703fa604fffcf2029a299076f
N4-Niraj <bhusalniraj03@gmail.com>
2026-08-04 21:52:01 +0545

    Configure SQLAlchemy engine and database session

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:123123@localhost:5432/student_management"
+
+engine = create_engine(DATABASE_URL)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
