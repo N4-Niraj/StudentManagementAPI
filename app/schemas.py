2f5b0baaf8ede00b2a73f97db1c887646daf00c9
N4-Niraj <bhusalniraj03@gmail.com>
2026-08-29 19:48:39 +0545

    feat: improve student update validation

--- app/schemas.py
+++ app/schemas.py
@@ -19,10 +19,10 @@ class StudentResponse(BaseModel):
     }
     
 class StudentUpdate(BaseModel):
-    name: str
-    faculty: str
-    semester: int
-    email: EmailStr
+    name: str = Field(min_length=2, max_length=100)
+    faculty: str = Field(min_length=2, max_length=100)
+    semester: int = Field(ge =1, le =8)
+    email: EmailStr 
     
 class UserCreate(BaseModel):
     email: EmailStr
