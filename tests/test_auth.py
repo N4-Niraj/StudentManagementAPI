b81ddb204943d45bcf085d05127b34def2e22e1c
N4-Niraj <bhusalniraj03@gmail.com>
2026-09-02 19:07:37 +0545

    test: add authentication tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -36,3 +36,95 @@ def test_register_duplicate_email(client):
     assert second_response.status_code == 409
     
     
+def test_login_success(client):
+    client.post(
+        "/auth/register",
+        json={
+            "email":"login11@gmail.com",
+            "password":"password2231"
+        }
+    )
+    response = client.post(
+        "/auth/login",
+        data={
+            "username": "login11@gmail.com",  #username instead of email cz OAuth2PasswordRequestForm expects username and password
+            "password": "password2231"
+            
+        }
+    )
+    
+    assert response.status_code == 200
+    data = response.json()
+    
+    assert "access_token" in data
+    assert data["token_type"] == "bearer"
+    
+def test_login_wrongPassword(client):
+    
+    client.post(
+        "/auth/register",
+        json={
+            "email":"testwrong@gmail.com",
+            "password":"password123123"
+        }
+    )
+    
+    response = client.post(
+        "/auth/login",
+        data={
+            "username":"testwrong@gmail.com",
+            "password":"wronggg"
+        }
+    )
+    
+    assert response.status_code == 401
+
+def test_login_unknownUser(client):
+    
+    response = client.post(
+        "/auth/login",
+        data={
+            "username": "unknown@gmail.com",
+            "password": "password"
+        }
+    )
+    
+    assert response.status_code == 401
+    
+def test_getme_authenticated(client):
+    
+    client.post(
+        "/auth/register",
+        json={
+            "email":"testauthenticated@gmail.com",
+            "password":"password123123"
+        }
+    )
+    
+    response = client.post(
+        "/auth/login",
+        data={
+            "username":"testauthenticated@gmail.com",
+            "password":"password123123"
+        }
+    )
+    
+    assert response.status_code == 200
+    
+    token = response.json()["access_token"]
+    
+    response = client.get(
+        "/users/me",
+        headers={
+            "Authorization": (f"bearer {token}")
+        }
+    )
+    
+    assert response.status_code == 200
+    assert response.json()["email"] == "testauthenticated@gmail.com"
+    
+
+def test_get_me_unauthenticated(client):
+    response = client.get("/users/me")
+
+    assert response.status_code == 401
