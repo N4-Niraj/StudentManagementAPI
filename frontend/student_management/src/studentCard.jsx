90256b459effb05313e873b1273038d30f54df75
N4-Niraj <bhusalniraj03@gmail.com>
2026-09-12 20:21:00 +0545

    remove unused frontend

--- frontend/student_management/src/studentCard.jsx
+++
@@ -1,9 +0,0 @@
-function StudentCard() {
-    return (
-                    <div className="text-lg font-bold text-left border border-gray-300 rounded-lg p-4 mb-4 w-80 " key={student.id}>
-                        <h3 > {student.name} </h3>
-                    </div>
-    )
-}
-
-export default StudentCard
